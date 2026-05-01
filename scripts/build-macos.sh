96b8b013e8e3acd7cbc3084840367722b861867b
Michael Sjöberg <mail@michaelsjoeberg.com>
2026-05-01 23:32:18 +0800

    Update build-macos.sh

--- scripts/build-macos.sh
+++ scripts/build-macos.sh
@@ -1,49 +1,47 @@
 #!/bin/sh
 set -eu
 
-version="${1:-0.1.0}"
-
 rm -rf dist
 mkdir -p dist/build dist/pkg
 
-xcrun clang -O2 tl.c \
+xcrun clang -O2 keypress.c \
     -framework ApplicationServices \
     -framework CoreFoundation \
     -target arm64-apple-macos11 \
-    -o dist/build/tl-arm64
+    -o dist/build/keypress-arm64
 
-xcrun clang -O2 tl.c \
+xcrun clang -O2 keypress.c \
     -framework ApplicationServices \
     -framework CoreFoundation \
     -target x86_64-apple-macos10.13 \
-    -o dist/build/tl-x86_64
+    -o dist/build/keypress-x86_64
 
 lipo -create \
-    dist/build/tl-arm64 \
-    dist/build/tl-x86_64 \
-    -output dist/build/tl
+    dist/build/keypress-arm64 \
+    dist/build/keypress-x86_64 \
+    -output dist/build/keypress
 
-chmod +x dist/build/tl-arm64
-chmod +x dist/build/tl-x86_64
-chmod +x dist/build/tl
+chmod +x dist/build/keypress-arm64
+chmod +x dist/build/keypress-x86_64
+chmod +x dist/build/keypress
 
-mkdir -p dist/pkg/tl-macos-arm64
-cp dist/build/tl-arm64 dist/pkg/tl-macos-arm64/tl
-cp README.md LICENSE dist/pkg/tl-macos-arm64/
+mkdir -p dist/pkg/keypress-macos-arm64
+cp dist/build/keypress-arm64 dist/pkg/keypress-macos-arm64/keypress
+cp README.md LICENSE dist/pkg/keypress-macos-arm64/
 
-mkdir -p dist/pkg/tl-macos-x86_64
-cp dist/build/tl-x86_64 dist/pkg/tl-macos-x86_64/tl
-cp README.md LICENSE dist/pkg/tl-macos-x86_64/
+mkdir -p dist/pkg/keypress-macos-x86_64
+cp dist/build/keypress-x86_64 dist/pkg/keypress-macos-x86_64/keypress
+cp README.md LICENSE dist/pkg/keypress-macos-x86_64/
 
-mkdir -p dist/pkg/tl-macos-universal
-cp dist/build/tl dist/pkg/tl-macos-universal/tl
-cp README.md LICENSE dist/pkg/tl-macos-universal/
+mkdir -p dist/pkg/keypress-macos-universal
+cp dist/build/keypress dist/pkg/keypress-macos-universal/keypress
+cp README.md LICENSE dist/pkg/keypress-macos-universal/
 
 cd dist/pkg
 
-tar -czf ../tl-macos-arm64.tar.gz tl-macos-arm64
-tar -czf ../tl-macos-x86_64.tar.gz tl-macos-x86_64
-tar -czf ../tl-macos-universal.tar.gz tl-macos-universal
+tar -czf ../keypress-macos-arm64.tar.gz keypress-macos-arm64
+tar -czf ../keypress-macos-x86_64.tar.gz keypress-macos-x86_64
+tar -czf ../keypress-macos-universal.tar.gz keypress-macos-universal
 
 cd ../..
 
